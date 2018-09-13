7258b6d16b4c1f0e3150616dc6a6722c7f8c6d4f
Robin Tram <robin.tram@gmail.com>
2018-09-13 18:19:50 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == phrase.upcase
+  if phrase == phrase.upcase
     return "NO, NO SINCE 1938!"
   else
     return "HUH?! SPEAK UP, SONNY!"  
